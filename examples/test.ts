b0c6b21e60dfe10b05aa49acb3bfd8941f81f827
Mattias Holmlund <mattias@holmlund.se>
2018-02-11 22:24:49 +0100

    Initial commit

---
+++ examples/test.ts
@@ -0,0 +1,18 @@
+import * as rpc from '../index'
+
+let server = new rpc.RPCServer()
+server.listen(12345, '127.0.0.1')
+server.on('client', (client: rpc.RPCClient) => {
+    client.on('message', data => {
+        console.log('Server received', data)
+        client.sendMessage({ test: 'back' })
+    })
+})
+
+setTimeout(() => {
+    let client = new rpc.RPCClient(12345, '127.0.0.1')
+    client.on('message', data => {
+        console.log('Client received', data)
+    })
+    client.sendMessage({ test: 1 })
+}, 1000)
---
+++ index.ts
@@ -0,0 +1,57 @@
+import * as net from 'net'
+import * as readline from 'readline'
+import { EventEmitter } from 'events'
+
+export class RPCServer extends EventEmitter {
+    private server: net.Server
+    constructor() {
+        super()
+        this.server = new net.Server()
+    }
+
+    listen(port: number, ip: string) {
+        this.server.listen(port, ip)
+        this.server.on('connection', (client: net.Socket) =>
+            this.newClient(client)
+        )
+    }
+
+    newClient(socket: net.Socket) {
+        this.emit('client', new RPCClient(socket))
+    }
+}
+
+export class RPCClient extends EventEmitter {
+    private socket: net.Socket
+    private rl: readline.ReadLine
+
+    constructor(port: number, ip: string)
+    constructor(socket: net.Socket)
+    constructor(p1: net.Socket | number, p2?: string) {
+        super()
+        if (typeof p1 === 'number' && typeof p2 === 'string') {
+            this.socket = new net.Socket()
+            this.socket.connect(p1, p2)
+        } else {
+            this.socket = p1 as net.Socket
+        }
+
+        this.rl = readline.createInterface(this.socket)
+        this.rl.on('line', line => this.receive(line))
+    }
+
+    sendMessage(message: any) {
+        this.socket.write(JSON.stringify({ t: 'msg', d: message }) + '\n')
+    }
+
+    private receive(line: string) {
+        let data = JSON.parse(line)
+        switch (data.t) {
+            case 'msg':
+                this.emit('message', data.d)
+                break
+            default:
+                throw `Unexpected data ${data}`
+        }
+    }
+}
